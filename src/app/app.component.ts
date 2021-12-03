07f8322f08abe0a845c43a06be8cff2b150c22f1
naomy <naodalinda@gmail.com>
2021-12-03 11:30:49 -0600

    subida de examen final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'examenFinal';
+export class AppComponent implements OnInit{
+  title="examenFinal"
+  cantidad=1;
+  total: number=0;
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+  calcular(operacion:string)
+  {
+    switch(operacion){
+      case 'a':
+        this.total=30*this.cantidad;
+      break;
+      case 'b':
+        this.total=40*this.cantidad;
+      break;
+      case 'c':
+        this.total=50*this.cantidad;
+      break;
+      default:
+      break;
+    }
+  }
+
+
 }
+    
